2989c0612be1a56c3c8051498affecb177378b4a
Bert <info@bertramoeler.de>
2022-09-17 21:47:16 +0200

    reworked my own function for multiply_arrays.py. Also added unittests for both functions.

--- multiply_arrays.py
+++ multiply_arrays.py
@@ -9,17 +9,11 @@ def multiply_arr(arr: list):
     n = len(arr)
     left, right = [1] * n, [1] * n
 
-    print(f'left: {left}, right: {right}')
-
     for i in range(1, n):
         left[i] = left[i - 1] * arr[i - 1]
 
-    print(left)
-
     for i in range(1, n):
         right[i] = right[i - 1] * arr[::-1][i - 1]
-
-    print(right[::-1])
 
     multiplied_arr = []
     for i in range(n):
@@ -36,16 +30,27 @@ def multiply_arr_2(arr):
     for i in range(n):
         product_all = product_all * arr[i]
 
-    multiplied_arr = []
+    nonzero_product = 1
     for i in range(n):
-        multiplied_arr.append(int(product_all / arr[i]))
+        if arr[i] != 0:
+            nonzero_product = nonzero_product * arr[i]
+
+    multiplied_arr = []
+    if arr.count(0) <= 1:
+        for i in range(n):
+            try:
+                multiplied_arr.append(int(product_all / arr[i]))
+            except ZeroDivisionError:
+                multiplied_arr.append(nonzero_product)
+    else:
+        multiplied_arr = [0] * n
 
     return multiplied_arr
 
 
 if __name__ == '__main__':
-    arr = [2, 3, 2, 5, 2]
+    arr = [1, 2, 3, 4]
     print(f'youtube method: \n'
           f'{multiply_arr(arr)}')
-    print(f'my method: multipl \n'
+    print(f'my method: \n'
           f'{multiply_arr_2(arr)}')
---
+++ test_multiply_arrays.py
@@ -0,0 +1,55 @@
+import unittest
+
+import multiply_arrays
+
+
+class TestMultiplyArrays(unittest.TestCase):
+
+    def setUp(self) -> None:
+        n = 4
+        self.positive_numbers = [*range(1, n + 1)]
+        self.negative_numbers = [*range(-1, -n - 1, -1)]
+        self.numbers_one_zero = [*range(0, n + 1)]
+        self.zeros = [0] * n
+
+    # positive numbers
+    def test_multiply_arr(self):
+        self.assertEqual(
+            multiply_arrays.multiply_arr(self.positive_numbers),
+            [24, 12, 8, 6],
+        )
+        self.assertEqual(
+            multiply_arrays.multiply_arr(self.negative_numbers),
+            [-24, -12, -8, -6],
+        )
+        self.assertEqual(
+            multiply_arrays.multiply_arr(self.numbers_one_zero),
+            [24, 0, 0, 0, 0],
+        )
+        self.assertEqual(
+            multiply_arrays.multiply_arr(self.zeros),
+            [0, 0, 0, 0],
+        )
+
+    def test_multiply_arr2(self):
+        self.assertEqual(
+            multiply_arrays.multiply_arr_2(self.positive_numbers),
+            [24, 12, 8, 6],
+        )
+        self.assertEqual(
+            multiply_arrays.multiply_arr_2(self.negative_numbers),
+            [-24, -12, -8, -6],
+        )
+        self.assertEqual(
+            multiply_arrays.multiply_arr(self.numbers_one_zero),
+            [24, 0, 0, 0, 0],
+        )
+        self.assertEqual(
+            multiply_arrays.multiply_arr_2(self.zeros),
+            [0, 0, 0, 0],
+        )
+
+
+if __name__ == '__main__':
+
+    unittest.main()
